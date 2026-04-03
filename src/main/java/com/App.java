dd8da35f7f8ef3709a7de37c6b883f4ddd812f46
Harika-t634 <harikathunu.53@gmail.com>
2026-04-03 17:34:28 +0530

    new mvn build

---
+++ src/main/java/com/App.java
@@ -0,0 +1,12 @@
+package com;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        while(true) {
+        // Infinite loop to keep the application running
+    }
+    }
+
+    
+}
